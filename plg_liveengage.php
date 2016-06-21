1cc7d13aae31cff63936c5e967939405c0d56235
Doug Cone <nullvariable@gmail.com>
2016-06-21 16:58:18 -0400

    rework from module to plugin

---
+++ plg_liveengage.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Live Engage Module Entry Point
+ */
+// No direct access
+defined('_JEXEC') or die;
+
+jimport( 'joomla.plugin.plugin' );
+
+class plgLivePersonLiveEngage extends JPlugin
+{
+
+    var $accountnum;
+
+
+
+}
